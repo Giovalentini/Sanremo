9f21b2fef793d9f5a7502824e3b842fbca8bf504
Giovanni Valentini <valentini.giovanni96@gmail.com>
2022-07-06 20:12:34 +0200

    fix

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -27,55 +27,90 @@ lista_uri=['spotify:playlist:3a67CvngL5t42BVvLgg8u5',
            'spotify:playlist:2zHJQOP880x7aWTkZqqNog']
 
 anno_inizio=2010
-anno_fine = 2022
+anno_fine = anno_inizio + len(lista_uri) - 1
 years=[i for i in reversed(range(anno_inizio,anno_fine+1))]
 
 #lista_uri=['spotify:playlist:13TbnOluHonxsVvSWkL7YF'] #1990-2021 in un'unica playlist
 
 #get each song id, title and artist for all playlists
 def get_ids_from_playlist(lista_uri):
+    '''
+    Returns lists of song id, title and artist for all playlists
+    '''
     ids=[[] for _ in range(len(lista_uri))]
     track_list = [[] for _ in range(len(lista_uri))]
     artist_list = [[] for _ in range(len(lista_uri))]
+
     for j,uri in enumerate(lista_uri):
-        results = spotify.playlist(uri)
+        try:
+            results = spotify.playlist(uri)
+        except:
+            print(f"the playlist '{uri}' contains at least one song which no longer exists on spotify")
+
         for i in range(len(results['tracks']['items'])):
             ids[j].append(results['tracks']['items'][i]['track']['id'])
             track_list[j].append(results['tracks']['items'][i]['track']['name'])
             artist_list[j].append(results['tracks']['items'][i]['track']['artists'][0]['name'])
+
     return ids, track_list, artist_list
 
 ids_list, track_list, artist_list = get_ids_from_playlist(lista_uri)
 
-#adjust list length to include only bigs (some playlists include "esordienti" too)
+# Function to adjust list length to include only bigs (some playlists include rookies too)
+def exclude_rookies(bigs_per_year, tot_years):
+    '''
+    adjust list length to exclude rookies 
+    '''
+    for i in range(tot_years):
+        ids_list[i] = ids_list[i][:bigs_per_year[i]]
+        track_list[i] = track_list[i][:bigs_per_year[i]]
+        artist_list[i] = artist_list[i][:bigs_per_year[i]]
+
+    return ids_list, track_list, artist_list
+
 bigs_per_year = (24,26,24,24,19,22,18,19,11,13,14,14,14)
-tot_years = 13
-for i in range(tot_years):
-    ids_list[i]    = ids_list[i][:bigs_per_year[i]]
-    track_list[i]  = track_list[i][:bigs_per_year[i]]
-    artist_list[i] = artist_list[i][:bigs_per_year[i]]
+tot_years = len(lista_uri)
+
+ids_list, track_list, artist_list = exclude_rookies(bigs_per_year, tot_years)
+
 
 #now I have all bigs songs ids. Next step: extract track features and put them in a df
-sanremo_df = pd.DataFrame()
-for i,lista in enumerate(ids_list):
-    for track_id in lista:
-        try:
-            song = spotify.audio_features(track_id)
-            sanremo_df = sanremo_df.append({'danceability': song[0]['danceability'],
-                               'energy': song[0]['energy'],
-                               'key': song[0]['key'],
-                               'loudness': song[0]['loudness'],
-                               'mode': song[0]['mode'],
-                               'speechiness': song[0]['speechiness'],
-                               'acousticness': song[0]['acousticness'],
-                               'instrumentalness': song[0]['instrumentalness'],
-                               'liveness': song[0]['liveness'],
-                               'valence': song[0]['valence'],
-                               'tempo': song[0]['tempo'],
-                               'duration_ms': song[0]['duration_ms'],
-                               'time_signature': song[0]['time_signature'],
-                               'year': years[i]}, ignore_index=True)
-        except: print(track_id,song)
+
+def extract_song_features(ids_list):
+    '''
+    Returns a pandas dataframe with track features
+    '''
+    df = pd.DataFrame()
+
+    # iterate over the list of all songs ids
+    for i,lista in enumerate(ids_list):
+        
+        # iterate over the list of each year songs ids
+        for track_id in lista:
+
+            try:
+                song = spotify.audio_features(track_id)
+                df = df.append({'danceability': song[0]['danceability'],
+                                'energy': song[0]['energy'],
+                                'key': song[0]['key'],
+                                'loudness': song[0]['loudness'],
+                                'mode': song[0]['mode'],
+                                'speechiness': song[0]['speechiness'],
+                                'acousticness': song[0]['acousticness'],
+                                'instrumentalness': song[0]['instrumentalness'],
+                                'liveness': song[0]['liveness'],
+                                'valence': song[0]['valence'],
+                                'tempo': song[0]['tempo'],
+                                'duration_ms': song[0]['duration_ms'],
+                                'time_signature': song[0]['time_signature'],
+                                'year': years[i]}, ignore_index=True)
+
+            except:
+                print(track_id,song)
+    
+    return df
+
+sanremo_df = extract_song_features(ids_list=ids_list)
 
 #there's one track_id which doesn't work - 4WJVLEkcMMV2tHo1Bd65VN
 
