657a16f9465099877862c0aaea25b1f0682a28c6
Giovanni Valentini <valentini.giovanni96@gmail.com>
2023-01-28 02:09:17 +0100

    added artist count

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -1,15 +1,19 @@
 #import libraries
-import pandas as pd
+import logging
 import numpy as np
+import pandas as pd
+import pickle
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from utils import *
+from utils import SanremoDatasetCreator
 
 #setup of spotify API
 import os
 os.environ['SPOTIPY_CLIENT_ID'] = '65856b2827c94410a969dc084bcd1c13'
 os.environ['SPOTIPY_CLIENT_SECRET'] = '4a749940bc164fa3892c83e7a2bbfcbe'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+# setup logging
+logging.basicConfig(level=logging.INFO)
 
 #ids of all playlists - order from 2022 to 2010
 #3a67CvngL5t42BVvLgg8u5
@@ -20,48 +24,72 @@ lista_uri=['spotify:playlist:3a67CvngL5t42BVvLgg8u5',
            'spotify:playlist:29qBlT6rlJtpKlaYhaUuK3',
            'spotify:playlist:0HEsgzoiLv3sx8ZZkgfYVp',
            'spotify:playlist:21kNTX3BPkbyJT3Q8Ni8D5',
-           'spotify:playlist:0Z18gq4LmBHUYeAmfBY3GS',
+           'spotify:playlist:3jLuMYBCqx6hniolxG9DP6',
            'spotify:playlist:5BqEPtLYvo7VjSouADG9bL',
            'spotify:playlist:1bJLbLWtSeN3htfgR9mfat',
            'spotify:playlist:4cQgkg1XHs1xc7LnZotEWY',
            'spotify:playlist:5umuFbGnpR1esP6BxjAnTx',
            'spotify:playlist:2zHJQOP880x7aWTkZqqNog']
 
-anno_inizio=2010
-anno_fine = anno_inizio + len(lista_uri) - 1
-years=[i for i in reversed(range(anno_inizio,anno_fine+1))]
+start_year=2010
+end_year = start_year + len(lista_uri) - 1
+years=[i for i in reversed(range(start_year,end_year+1))]
+bigs_per_year = (24,26,24,24,19,22,18,17,11,13,14,14,14)
+tot_years = len(lista_uri)
 
-#lista_uri=['spotify:playlist:13TbnOluHonxsVvSWkL7YF'] #1990-2021 in un'unica playlist
-
-if __name__ == "__main__":
+def main():
+    """
+    This function runs the main code of the script.
+    It gets the ids, tracks, and artists of all playlists,
+    removes the rookies, extracts the song features,
+    adds the winner column and the title and artist columns
+    to the dataframe and returns the dataframe.
+    """
+    dataset_creator = SanremoDatasetCreator(
+        bigs_per_year = bigs_per_year,
+        tot_years = tot_years,
+        years = years
+    )
 
     # get list of ids, songs and artists from each playlist
-    ids_list, track_list, artist_list = get_ids_from_playlist(lista_uri)
+    logging.info(" Fetching playlists URIs...")
+    ids_list, track_list, artist_list, artist_count = dataset_creator.get_ids_from_playlist(lista_uri)
 
     # adjust lists removing rookies
-    bigs_per_year = (24,26,24,24,19,22,18,19,11,13,14,14,14)
-    tot_years = len(lista_uri)
-    ids_list, track_list, artist_list = exclude_rookies(bigs_per_year, tot_years)
+    logging.info(" Removing rookies...")
+    ids_list, track_list, artist_list, artist_count = dataset_creator.exclude_rookies(
+        ids_list, track_list, artist_list, artist_count
+    )
 
-    #now I have all bigs songs ids. Next step: extract track features and put them in a df
-    sanremo_df = extract_song_features(ids_list=ids_list)
+    #Extract track features and put them in a df
+    logging.info(" Extracting track features...")
+    sanremo_df = dataset_creator.extract_song_features(ids_list=ids_list)
 
     #there's one track_id which doesn't work - 4WJVLEkcMMV2tHo1Bd65VN
 
     #add winner column - first of each playlist is a winner except for 2021
-    sanremo_df = add_winner(sanremo_df)
+    logging.info(" Adding winner column...")
+    sanremo_df = dataset_creator.add_winner(sanremo_df)
 
     #add title and artist
-    sanremo_df = add_title_and_artist(sanremo_df)
+    logging.info(" Adding title and artist to each song...")
+    sanremo_df = dataset_creator.add_title_and_artist(
+        sanremo_df,track_list,artist_list,artist_count
+    )
 
-#add 2021 winner
-#sanremo_df.loc[sanremo_df['song']=='ZITTI E BUONI','winner']=1
+    return sanremo_df
 
-#manually impute artist type and sex
-# Type: 1 for solo artist, 2 for collab or duet, 3 for band
-# Sex: 0 for male, 1 for female, 2 for mix/other (considered frontman in bands to elige sex)
-#sanremo_df[['song','artist']].to_excel(r"C:\Users\g.valentini\Documents\Projects\Sanremo\src\artist_sex_type.xlsx")
-#artist_sex_type = pd.read_excel(r"C:\Users\g.valentini\Documents\Projects\Sanremo\src\artist_sex_type.xlsx")
-#sanremo_df = pd.merge(sanremo_df,artist_sex_type,on=['song','artist'])
+if __name__ == "__main__":
+    sanremo_df = main()
 
-#sanremo_df.to_excel(r"C:\Users\g.valentini\Documents\Projects\Sanremo\src\sanremo_df.xlsx")
+    #manually impute artist type and sex
+    # Type: 1 for solo artist, 2 for collab or duet, 3 for band
+    # Sex: 0 for male, 1 for female, 2 for mix/other (considered frontman in bands to elige sex)
+    #sanremo_df[['song','artist']].to_excel(r"C:\Users\g.valentini\Documents\Projects\Sanremo\src\artist_sex_type.xlsx")
+    #artist_sex_type = pd.read_excel(r"C:\Users\g.valentini\Documents\Projects\Sanremo\src\artist_sex_type.xlsx")
+    #sanremo_df = pd.merge(sanremo_df,artist_sex_type,on=['song','artist'])
+
+    with open("sanremo_df.pkl", "wb") as f:
+        pickle.dump(sanremo_df, f)
+
+    #sanremo_df.to_excel(r"C:\Users\valen\Documents\Projects\Sanremo\src\sanremo_df.xlsx", index=False)
--- src/utils.py
+++ src/utils.py
@@ -1,109 +1,136 @@
-# useful functions
+import logging
+import numpy as np
 import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-#get each song id, title and artist for all playlists
-def get_ids_from_playlist(lista_uri:list):
-    '''
-    Returns lists of song id, title and artist for all playlists
-    '''
-    ids=[[] for _ in range(len(lista_uri))]
-    track_list = [[] for _ in range(len(lista_uri))]
-    artist_list = [[] for _ in range(len(lista_uri))]
+class SanremoDatasetCreator:
 
-    for j,uri in enumerate(lista_uri):
-        results = spotify.playlist(uri)
-        results = {}
-        try:
-            results = spotify.playlist(uri)
-            print(results)
-        except:
-            print(f"the playlist '{uri}' contains at least one song which no longer exists on spotify")
-            pass
+    def __init__(self, bigs_per_year, tot_years, years):
+        self.spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+        self.bigs_per_year = bigs_per_year
+        self.tot_years = tot_years
+        self.years = years
 
-        for i in range(len(results['tracks']['items'])):
-            ids[j].append(results['tracks']['items'][i]['track']['id'])
-            track_list[j].append(results['tracks']['items'][i]['track']['name'])
-            artist_list[j].append(results['tracks']['items'][i]['track']['artists'][0]['name'])
+    def get_ids_from_playlist(self, lista_uri: list):
+        """
+        Returns lists of song id, title and artist for all playlists
+        """
+        ids = [[] for _ in range(len(lista_uri))]
+        track_list = [[] for _ in range(len(lista_uri))]
+        artist_list = [[] for _ in range(len(lista_uri))]
+        artist_count = [[] for _ in range(len(lista_uri))]
 
-    return ids, track_list, artist_list
-
-# Function to adjust list length to include only bigs (some playlists include rookies too)
-def exclude_rookies(bigs_per_year:tuple, tot_years:int):
-    '''
-    adjust list length to exclude rookies 
-    '''
-    for i in range(tot_years):
-        ids_list[i] = ids_list[i][:bigs_per_year[i]]
-        track_list[i] = track_list[i][:bigs_per_year[i]]
-        artist_list[i] = artist_list[i][:bigs_per_year[i]]
-
-    return ids_list, track_list, artist_list
-
-def extract_song_features(ids_list):
-    '''
-    Returns a pandas dataframe with track features
-    '''
-    df = pd.DataFrame()
-
-    # iterate over the list of all songs ids
-    for i,lista in enumerate(ids_list):
-        
-        # iterate over the list of each year songs ids
-        for track_id in lista:
-
+        for j, uri in enumerate(lista_uri):
+            results = self.spotify.playlist(uri)
+            results = {}
             try:
-                song = spotify.audio_features(track_id)
-                df = df.append({'danceability': song[0]['danceability'],
-                                'energy': song[0]['energy'],
-                                'key': song[0]['key'],
-                                'loudness': song[0]['loudness'],
-                                'mode': song[0]['mode'],
-                                'speechiness': song[0]['speechiness'],
-                                'acousticness': song[0]['acousticness'],
-                                'instrumentalness': song[0]['instrumentalness'],
-                                'liveness': song[0]['liveness'],
-                                'valence': song[0]['valence'],
-                                'tempo': song[0]['tempo'],
-                                'duration_ms': song[0]['duration_ms'],
-                                'time_signature': song[0]['time_signature'],
-                                'year': years[i]}, ignore_index=True)
-
+                results = self.spotify.playlist(uri)
             except:
-                print(track_id,song)
+                print(f"the playlist '{uri}' contains at least one song which no longer exists on spotify")
+                pass
+
+            for i in range(len(results['tracks']['items'])):
+                ids[j].append(results['tracks']['items'][i]['track']['id'])
+                track_list[j].append(results['tracks']['items'][i]['track']['name'])
+                artist_list[j].append(
+                    " & ".join([artist['name'] for artist in results['tracks']['items'][i]['track']['artists']])
+                )
+                artist_count[j].append(len(results['tracks']['items'][i]['track']['artists']))
+
+        return ids, track_list, artist_list, artist_count
+
+    def exclude_rookies(self, ids_list, track_list, artist_list, artist_count):
+        """
+        adjust list length to exclude rookies
+        """
+        for i in range(self.tot_years):
+            ids_list[i] = ids_list[i][:self.bigs_per_year[i]]
+            track_list[i] = track_list[i][:self.bigs_per_year[i]]
+            artist_list[i] = artist_list[i][:self.bigs_per_year[i]]
+            artist_count[i] = artist_count[i][:self.bigs_per_year[i]]
+
+        return ids_list, track_list, artist_list, artist_count
+
+    def extract_song_features(self, ids_list):
+        """
+        Returns a pandas dataframe with track features
+        """
+        df = pd.DataFrame()
+
+        # iterate over the list of all songs ids
+        for i, lista in enumerate(ids_list):
+
+            # iterate over the list of each year songs ids
+            for j,track_id in enumerate(lista):
+                
+                try:
+                    song = self.spotify.audio_features(track_id)
+                    df = df.append({'danceability':song[0]['danceability'],
+                                    'energy':song[0]['energy'],
+                                    'key':song[0]['key'],
+                                    'loudness':song[0]['loudness'],
+                                    'mode':song[0]['mode'],
+                                    'speechiness':song[0]['speechiness'],
+                                    'acousticness':song[0]['acousticness'],
+                                    'instrumentalness':song[0]['instrumentalness'],
+                                    'liveness':song[0]['liveness'],
+                                    'valence':song[0]['valence'],
+                                    'tempo':song[0]['tempo'],
+                                    'duration_ms':song[0]['duration_ms'],
+                                    'time_signature':song[0]['time_signature'],
+                                    'year': self.years[i]}, ignore_index=True)
+
+                except:
+                    logging.warning(f" missing element #{j} in list #{i}")
+
+        return df
+
+    def add_winner(self,df):
+        """
+        Add winner column  - first of each playlist is a winner
+        """
+        bigs_per_year_cum = np.cumsum(self.bigs_per_year)
+        bigs_per_year_cum = np.append(bigs_per_year_cum, 0)
+        df['winner']=0
+
+        for i in range(len(df)):
+            if i in bigs_per_year_cum:
+                df.loc[i,'winner'] = 1
+        
+        return df
+
+    #add title and artist
+    def add_title_and_artist(
+        self,
+        df:pd.DataFrame,
+        track_list:list,
+        artist_list:list,
+        artist_count:list
+    ) -> pd.DataFrame:
+        '''
+        Add title and artist to sanremo dataframe
+        '''
+        unique_track_list = []
+        unique_artist_list = []
+        unique_count_list = []
+
+        print(track_list)
+        print(artist_count)
+        for l1,l2,l3 in zip(track_list,artist_list,artist_count):
+            for track,artist,count in zip(l1,l2,l3):
+                unique_track_list.append(track)
+                unique_artist_list.append(artist)
+                unique_count_list.append(count)
+
+
+        # 20230128: removing one element without track features
+        #unique_track_list.remove("biggio e mandelli vita d inferno lyric video 4207191295023505344")
+        #unique_artist_list.remove("")
+
+        # add columns
+        df['song'] = unique_track_list
+        df['artist'] = unique_artist_list
+        df['number_of_artists'] = unique_count_list
     
-    return df
-
-#add winner column - first of each playlist is a winner except for 2021
-def add_winner(df):
-    '''
-    Add winner column  - first of each playlist is a winner except for 2021
-    '''
-    bigs_per_year_cum = np.cumsum(bigs_per_year)
-    df['winner']=0
-
-    for i in range(len(df)):
-        if i in bigs_per_year_cum:
-            df.loc[i,'winner'] = 1
-
-    return df
-
-#add title and artist
-def add_title_and_artist(
-    df:pd.DataFrame
-) -> pd.DataFrame:
-    '''
-    Add title and artist to sanremo dataframe
-    '''
-    unique_track_list = []
-    unique_artist_list = []
-
-    for l1,l2 in zip(track_list,artist_list):
-        for track,artist in zip(l1,l2):
-            unique_track_list.append(track)
-            unique_artist_list.append(artist)
-
-    # add columns
-    df['song']=unique_track_list
-    df['artist']=unique_artist_list
-
-    return df 
+        return df
